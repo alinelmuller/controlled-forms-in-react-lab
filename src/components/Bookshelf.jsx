542df816fcedd82962c57ccec04877edba5d22ad
alinelmuller <alinelmuller@gmail.com>
2024-10-31 14:24:49 -0600

    1 - Create component

--- src/components/Bookshelf.jsx
+++ src/components/Bookshelf.jsx
@@ -1,9 +1,18 @@
 import { useState } from 'react';
 
-<div className="bookshelfDiv">
-  <div className="formDiv">
-    <h3>Add a Book</h3>
-    {/* Form will go here */}
-  </div>
-  <div className="bookCardsDiv">{/* Book cards will display here */}</div>
-</div>
+
+
+
+const Bookshelf = () => {
+    return (
+        <div className="bookshelfDiv">
+            <div className="formDiv">
+                <h3>Add a Book</h3>
+                {/* Form will go here */}
+            </div>
+            <div className="bookCardsDiv">{/* Book cards will display here */}</div>
+        </div>
+    );
+};
+  
+export default Bookshelf;
