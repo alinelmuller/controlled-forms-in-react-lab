e357f91402267f26f68e5126a9f125cb431b72cf
alinelmuller <alinelmuller@gmail.com>
2024-10-31 14:55:47 -0600

    4 - Create function handleSubmit

--- src/components/Bookshelf.jsx
+++ src/components/Bookshelf.jsx
@@ -15,17 +15,24 @@ const Bookshelf = () => {
         setNewBook({...newBook, [event.target.name] : event.target.value});
     }
 
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        setBooks([...books, newBook]);
+        setNewBook({ title: '', author: '' });
+    };
+
     return (
         <div className="bookshelfDiv">
             <div className="formDiv">
                 <h3>Add a Book</h3>
-                <form> 
+                <form onSubmit={handleSubmit}> 
                     <label htmlFor="title">Title: </label>
                     <input id="title" name="title" value={newBook.title} onChange={handleInputChange}/>
                     <br />
                     <label htmlFor="author">Authour: </label>
                     <input id="author" name="author" value={newBook.author} onChange={handleInputChange} />
                     <br />
+                    <button type="submit">Submit your name</button>
                 </form>
             </div>
             <div className="bookCardsDiv">{/* Book cards will display here */}</div>
