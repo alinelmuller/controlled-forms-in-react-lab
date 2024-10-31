45c39eb6420bfac18df448b4e81de9013bd63c30
alinelmuller <alinelmuller@gmail.com>
2024-10-31 14:43:13 -0600

    3 - Create function handleInputChange

--- src/components/Bookshelf.jsx
+++ src/components/Bookshelf.jsx
@@ -5,17 +5,28 @@ const Bookshelf = () => {
         { title: 'Fourth Wing', author: 'Rebecca Yarros' },
         { title: 'The Lion, the Witch and the Wardrobe', author: 'C.S. Lewis' },
     ]);
+
     const [newBook , setNewBook] = useState ({
         title: '',
         author: '',
     })
-
+    
+    const handleInputChange = (event) => {
+        setNewBook({...newBook, [event.target.name] : event.target.value});
+    }
 
     return (
         <div className="bookshelfDiv">
             <div className="formDiv">
                 <h3>Add a Book</h3>
-                {/* Form will go here */}
+                <form> 
+                    <label htmlFor="title">Title: </label>
+                    <input id="title" name="title" value={newBook.title} onChange={handleInputChange}/>
+                    <br />
+                    <label htmlFor="author">Authour: </label>
+                    <input id="author" name="author" value={newBook.author} onChange={handleInputChange} />
+                    <br />
+                </form>
             </div>
             <div className="bookCardsDiv">{/* Book cards will display here */}</div>
         </div>
