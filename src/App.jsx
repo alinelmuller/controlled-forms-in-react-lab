cc092095e9f6165ff17b8d15ea3c0dde64bbd4e0
alinelmuller <alinelmuller@gmail.com>
2024-10-31 14:24:22 -0600

    1 - Create component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
-import './index.css'
-import { useState } from 'react'
-
-
+// src/App.jsx
+import './App.css';
+import Bookshelf from './components/Bookshelf.jsx';
 
 const App = () => {
-  return <h1>Hello world!</h1>;
+  return (
+    <>
+      <h1>My Bookshelf</h1>
+      <Bookshelf />
+    </>
+  );
 };
 
 export default App;
